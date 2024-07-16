8ebc2fd0b30b348753b7da9b8d6f13ecaf23b6ec
achintha98 <kalanaachintha@gmail.com>
2024-07-16 22:32:53 +0530

    Implemented basic transfer logic without concurrent access safty

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -12,4 +12,41 @@ accoutRouter.get("/balance", accountMiddleware, async (req, res) => {
     })
 })
 
+accoutRouter.post("/transfer", accountMiddleware, async (req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    if(account.balance < req.body.amount) {
+        return res.status(400).json({
+            message: "Insufficient balance"
+        })
+    }
+
+    const toAccount = await Account.findOne({
+        userId: req.body.to
+    })
+
+    if (!toAccount) {
+        return res.status(400).json({
+            message: "Invalid account"
+        })
+    }
+
+    await Account.updateOne({
+        userId: req.userId,
+        balance: account.balance - req.body.amount
+    })
+
+    await Account.updateOne({
+        userId: req.body.to,
+        balance: toAccount.balance + req.body.amount
+    })
+
+    return res.json({
+            message: "Transfer successful"
+    })
+    
+})
+
 module.exports = accoutRouter;
