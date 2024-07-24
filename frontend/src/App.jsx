46e83789da12b381ca8bc6748418501bb11059c5
achintha98 <kalanaachintha@gmail.com>
2024-07-24 23:25:30 +0530

    Added routes to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Send from "./pages/Send";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <>
+      <h1>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/send" element={<Send />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
